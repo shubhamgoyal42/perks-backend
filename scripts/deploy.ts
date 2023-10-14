45f58ce7f97c8b95ad74e5bed4cd8a15472da927
Shubham Goyal <sgshubham@gmail.com>
2023-10-14 12:52:27 +0530

    Deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,65 @@
-import { ethers } from "hardhat";
+import hardhat,  { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+    const [admin, nftOwner1] = await ethers.getSigners()
+    let tx
+    // console.log(admin.address)
+    // console.log(nftOwner1.address)
 
-  const lockedAmount = ethers.parseEther("0.001");
+    // const perksNFT = await ethers.deployContract("PerksNFT", []);
+    // console.log(perksNFT.address)
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+    const perksNFT = await ethers.getContractAt("PerksNFT", "0x8c1a55B3c6629716571234A1935ad2F593853066")
 
-  await lock.waitForDeployment();
+    // const tx = await perksNFT.safeMint(nftOwner1.address, 1)
+    // console.log(tx)
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+    // const registry = await ethers.deployContract("ERC6551Registry", []);
+    // console.log(registry.address)
+
+    // const accountImpl = await ethers.deployContract("ERC6551Account", []);
+    // console.log(accountImpl.address)
+
+    const registry = await ethers.getContractAt("ERC6551Registry", "0x5911C7265d0f16964821D58879e34Cdc7e47Ae7F")
+    const accountImpl = await ethers.getContractAt("ERC6551Account", "0x6C236B327Fa1eaE7808dB029DdF69de19fAB5EfD")
+
+    // const tbaAddr = await registry.account(accountImpl.address, 11155111, perksNFT.address, 1, 0)
+    // console.log({tbaAddr})
+
+    // tx = await registry.createAccount(accountImpl.address, 11155111, perksNFT.address, 1, 0, [])
+    // console.log({tx})
+
+    const tba1 = await ethers.getContractAt("ERC6551Account", "0x4Ef36E88D565E750Da610590F5466F06b3Cb6A49")
+    // console.log({tba1})
+
+    const owner = await tba1.owner()
+    console.log({owner})
+    const token = await tba1.token()
+    console.log({token})
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
+
+
+// ----------- SEPOLIA -------------
+// NFT contract
+// 0x8c1a55B3c6629716571234A1935ad2F593853066
+
+// NFT id 1
+// 0xE671902678F3F1Cd0320EAd6182C8b0D7c04e045
+// pk f2b0d0d488b324545d32b0a82e6a0097fffa6c7e0cc794d1e8ad515f5331a1fc
+
+// ERC6551Registry
+// 0x5911C7265d0f16964821D58879e34Cdc7e47Ae7F
+
+// ERC6551Account
+// 0x6C236B327Fa1eaE7808dB029DdF69de19fAB5EfD
+
+// precomputed tba address
+// 0x4Ef36E88D565E750Da610590F5466F06b3Cb6A49
