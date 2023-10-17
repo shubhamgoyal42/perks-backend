f39c4a08824a25e70aadf0dddb2b420c6c26ee8d
Shubham Goyal <sgshubham@gmail.com>
2023-10-17 17:22:39 +0530

    Modify hardhat config and add gitmodule

--- contracts/USDC.sol
+++ contracts/USDC.sol
@@ -5,25 +5,14 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PerksToken is ERC20, Ownable {
-    address public vault;
-
+contract USDC is ERC20, Ownable {
     uint256[50] private __gap;
 
     constructor () ERC20('PerksToken', 'PRKS') Ownable(msg.sender) {
         _mint(msg.sender, 1000000 * 10 ** decimals());  // 1M
     }
 
-    function setVault(address _vault) external onlyOwner {
-        vault = _vault;
-    }
-
     function mint(address account, uint256 value) external onlyOwner {
         _mint(account, value);
-    }
-
-    function burn(address account, uint256 value) external  {
-        require(msg.sender == vault, "Only vault can burn");
-        _burn(account, value);
     }
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,59 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";  // Required for deploying contracts, etc.
+// import "hardhat-preprocessor";
+// import fs from "fs";
+
+const pk = process.env.PRIVATE_KEY || ''
+
+// function getRemappings() {
+//   return fs
+//     .readFileSync("remappings.txt", "utf8")
+//     .split("\n")
+//     .filter(Boolean) // remove empty lines
+//     .map((line) => line.trim().split("="));
+// }
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  networks: {
+    goerli: {
+      url: "https://goerli.infura.io/v3/06800f8293644a29b29acbc1148042f1",
+      accounts: [ pk ],
+      chainId: 5,
+      gasPrice: 20000000000,
+    },
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/06800f8293644a29b29acbc1148042f1",
+      accounts: [
+        pk, // admin
+        "f2b0d0d488b324545d32b0a82e6a0097fffa6c7e0cc794d1e8ad515f5331a1fc" // nft #1 holder
+      ],
+      chainId: 11155111,
+      // gasPrice: 20000000000, // You can adjust the gas price or other parameters if necessary
+    }
+  },
+
+  // preprocess: {
+  //   eachLine: (hre) => ({
+  //     transform: (line: string) => {
+  //       if (line.match(/^\s*import /i)) {
+  //         for (const [from, to] of getRemappings()) {
+  //           if (line.includes(from)) {
+  //             line = line.replace(from, to);
+  //             break;
+  //           }
+  //         }
+  //       }
+  //       return line;
+  //     },
+  //   }),
+  // },
+  // paths: {
+  //   sources: "./src",
+  //   cache: "./cache_hardhat",
+  // },
+
 };
 
 export default config;
